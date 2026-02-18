e9ddf72c33da78d82cfae1db739a5836e0730a7d
Shailove <124027365+shailovesingh@users.noreply.github.com>
2026-02-18 18:48:47 +0530

    git commit -m "fix: update state when graph is updated"

--- backend/src/graph/state.py
+++ backend/src/graph/state.py
@@ -1,4 +1,24 @@
 import operator
 from typing import Annotated, List, Dict, Optional, Any, TypedDict
 
-# Defin a 
+# Defin the schema for a single  compliance result
+
+class ComplianceIssue(TypedDict):
+    category: str   
+    description: str    # Specific detail of violation
+    severity: str   # Critical | Warning
+    timestamp: Optional[str]
+
+# define the global graph state
+class VideoAudioState(TypedDict):
+    '''
+    Defines the data schema for langgraph execution content
+    '''
+
+    # Input parameters
+    video_url: str
+    video_id: str
+
+    # Ingestion and extraction data
+    local_file_path: Optional[str]
+    video_metadata: Dict[str,Any]
