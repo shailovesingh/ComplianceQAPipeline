54773825cbec0c309fe8c0a43ff9393a735b7572
Shailove <124027365+shailovesingh@users.noreply.github.com>
2026-02-23 23:18:56 +0530

    git commit -m "create main.py file"

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
-def main():
-    print("Hello from complianceqapipeline!")
+'''
+Main execution entry point for the Compliance QA Pipeline.
+'''
 
+import uuid
+import json
+import logging
+from pprint import pprint
 
-if __name__ == "__main__":
-    main()
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+from backend.src.graph.workflow import app
+
+logging.basicConfig(
+    level = logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger("brand-guardian-runner")
+
+def run_cli_simulation():
+    '''
+    Simulates the video compliance audit request
+    '''
+
+    # generate the session ID
+    session_id = str(uuid.uuid4())
+    logger.info(f"Starting Audit Session: {session_id}")
+
+    # define the initial state
+    initial_inputs = {
+        "video_url" : "",
+        "video_id": f"vid_{session_id[:8]}",
+        "compliance_results" : [],
+        "errors": []
+    }
+
+    print("n----Initializing workflow.........")
+    print(f"Input Payload : {json.dumps(initial_inputs, indent=2)}")
+
+    try:
+        final_state = app.invoke(initial_inputs)
+        print("\n-----Workflow execution is complete......")
+
+        print("\n Compliance Audit Report ==")
+        print(f"Video ID: {final_state.get('final_status')}")
+        print(f"Status : {final_state.get('final_status')}")
+        print("\n [VIOLATIONS DETECTED]")
+        results = final_state.get('compliance_results', [])
+        if results:
+            for issue in results:
+                print(f"-[{issue.get('severity')}] [{issue.get('category')}] : [{issue.get('description')}]")
+        else:
+            print("No Violations detected....")
+
+        print("\n[FINAL SUMMARY]")
+        print(final_state.get('final_report'))
+
+    except Exception as e:
+        logger.error(f"Workflow Execution Failed : {str(e)}")
+
+if __name__ == "main":
+    run_cli_simulation()
