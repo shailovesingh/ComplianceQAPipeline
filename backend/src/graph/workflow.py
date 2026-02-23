43a52afbe0339398d76acdb5d60b3625a3ff5ff2
Shailove <124027365+shailovesingh@users.noreply.github.com>
2026-02-23 17:34:20 +0530

    git commit -m "workflow"

---
+++ backend/src/graph/workflow.py
@@ -0,0 +1,30 @@
+'''
+This module defines the DAG: Directed Acyclic Graph that orchestrates the video compliance
+audit process.
+It connects the nodes using the StateGraph from Langgraph
+
+START -> index_video_node -> auit_content_node -> END
+'''
+
+from langgraph.graph import StateGraph, END
+from backend.src.graph.state import VideoAuditState
+
+from backend.src.graph.nodes import (
+    index_video_node,
+    audio_content_node
+)
+
+def create_graph():
+    '''
+    Constructs and compiles the LangGraph Workflow
+    Returns:
+    Compiled Graph: runnable graph object for execution
+    '''
+
+    # Initialize the graph with state schema
+    workflow = StateGraph(VideoAuditState)
+    # add the nodes
+    workflow.add_node("indexer", index_video_node)
+    workflow.add_node("auditor", audio_content_node)
+    # define the entry point
+    
